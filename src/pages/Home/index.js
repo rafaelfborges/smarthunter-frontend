e803c3e73afee4653f3378051560d3387f1649e9
Rafael Borges <rafaelfborges@gmail.com>
2021-05-03 16:18:49 -0300

    Fix problems with auto import

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,10 @@
+import React from "react";
+import NavBar from "../../components/NavBar";
+
 export default function Home() {
   return (
     <>
+      <NavBar />
       <div>
         <h1>Home Page Content</h1>
       </div>
