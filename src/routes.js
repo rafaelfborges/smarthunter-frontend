24ce50ea48b1c65f4e8e9a0dbc1413f4b4308586
Rafael Borges <rafaelfborges@gmail.com>
2021-05-12 15:15:19 -0300

    Add exact path to home route

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
       </Switch>
     </Router>
   );
