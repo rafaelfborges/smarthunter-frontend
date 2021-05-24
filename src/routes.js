7dfb79035ae36da82480c5d509c22a11bc756a63
Rafael Borges <rafaelfborges@gmail.com>
2021-05-24 19:14:01 -0300

    Add pages and private routes

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,28 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import PrivateRoute from "./components/PrivateRoute";
+
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Courses from "./pages/Courses";
+import PageNotFound from "./pages/404";
+import EnrolledCourses from "./pages/EnrolledCourses";
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Home} />
+        <PrivateRoute path="/" component={Home} exact />
+        <PrivateRoute path="/courses" component={Courses} exact />
+        <PrivateRoute
+          path="/enrolled_courses"
+          component={EnrolledCourses}
+          exact
+        />
+        <Route path="/login" component={Login} exact />
+        <Route path="/signup" component={Signup} exact />
+        <Route path="*" component={PageNotFound} />
       </Switch>
     </Router>
   );
