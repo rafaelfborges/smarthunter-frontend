4cf5336b3fe5e353673878fe025a250bbe1034c7
Rafael Borges <rafaelfborges@gmail.com>
2021-05-31 22:22:39 -0300

    Update loading component

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Container, Image, Spinner } from "react-bootstrap";
+
+import Logo from "../../assets/images/logo.png";
 
 export default function Loading() {
   return (
-    <>
-      <h1>Loading</h1>
-    </>
+    <Container
+      className="d-flex flex-column align-items-center justify-content-center min-vw-100 min-vh-100"
+      fluid
+    >
+      <Image src={Logo} className="logo-static" />
+      <Spinner animation="border" variant="warning" className="mt-5" />
+    </Container>
   );
 }
