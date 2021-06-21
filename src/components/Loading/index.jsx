f292222609e5fa9e73c2a321809b9c7e239de9ba
Rafael Borges <rafaelfborges@gmail.com>
2021-06-21 00:23:27 -0300

    Fix loading with paramaters for spinner only

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,22 +3,23 @@ import {Container, Image, Spinner} from "react-bootstrap";
 
 import Logo from "../../assets/images/logo.png";
 
-export default function Loading(onlySpinner) {
-  
-  return onlySpinner ? (
+export default function Loading({ onlySpinner }) {
+    
+  return (
     <Container
       className="d-flex flex-column align-items-center justify-content-center min-vw-100 min-vh-100"
       fluid
     >
-      <Spinner animation="border" variant="warning" className="mt-5"/>
-    </Container>
-  ) : (
-    <Container
-      className="d-flex flex-column align-items-center justify-content-center min-vw-100 min-vh-100"
-      fluid
-    >
-      <Image src={Logo} className="logo-static"/>
-      <Spinner animation="border" variant="warning" className="mt-5"/>
+      { 
+        onlySpinner ? (
+          <Spinner animation="border" variant="warning" className="mt-5"/>
+        ) : (
+          <>
+            <Image src={Logo} className="logo-static"/>
+            <Spinner animation="border" variant="warning" className="mt-5"/>
+          </>
+        )
+      }
     </Container>
   );
 }
